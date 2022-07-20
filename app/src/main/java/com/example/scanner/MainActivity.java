a1e7c6dcd55835106fe74054d2bcf65c0c5ed139
IbrahimKhaliliye <97684777+IbrahimKhaliliye@users.noreply.github.com>
2022-07-20 10:26:34 +0300

    fifth commit

--- app/src/main/java/com/example/scanner/MainActivity.java
+++ app/src/main/java/com/example/scanner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.scanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,26 +10,74 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    Button scanButton;
+    private EditText email, password;
+    private Button sign_in, sign_up;
+    private Button scanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        sign_in = findViewById(R.id.sign_in);
+        sign_up = findViewById(R.id.sign_up);
         scanButton= findViewById(R.id.scanButton);
         scanButton.setOnClickListener(this);
+        sign_in.setOnClickListener(this);
+        sign_up.setOnClickListener(this);
     }
     @Override
     public void onClick(View view) {
-        scancode();
+        if (sign_in == view) {
+            signin(email.getText().toString(), password.getText().toString());
+
+        }
+        if (sign_up == view) {
+            Intent intent1 = new Intent(this, SignupActivity.class);
+            startActivity(intent1);
+        }
+        if (scanButton == view ){
+            Intent intent2 = new Intent(this,)
+
+        }
     }
+
+
+
+
+    }
+
+    public void signin(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(MainActivity.this, ContentActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+
+
+
 
 
     private void scancode(){
