de53bd4ab0f480086506549dcbed5b438c402306
Prox-A <amirkh610@gmail.com>
2022-07-21 15:52:49 +0300

    adding Home

---
+++ app/src/main/java/com/example/scanner/Home.java
@@ -0,0 +1,25 @@
+package com.example.scanner;
+
+import android.app.Application;
+import android.content.Intent;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/* The use of this class is to keep the user logged in after they leave the app */
+
+public class Home extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser FireUser = mAuth.getCurrentUser();
+
+        if(FireUser != null) {
+            Intent intent = new Intent(Home.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+    }
+}
